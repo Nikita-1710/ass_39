4d8fc8977877745fff136ba9ebadded4fc672616
Nikita-1710 <chausalkarnikita@gmail.com>
2024-12-25 14:39:32 +0530

    functions for boxes created

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 const board = [-1, -1, -1, -1, -1, -1, -1, -1, -1]
+let currentPlayer = "X"
 
 function rendorBoard() {
    const allBoxes = document.querySelectorAll('.box')
    
    allBoxes.forEach((boxElement, i) => {
-    if (board[i] === "X") {
-        boxElement.innerHTML = "X"
+    if (board[i] == "O") {
+        boxElement.innerHTML = '<img src="./img/O.png" height="50px"/>'
     }
-    else if (board[i] === "O") {
-        boxElement.innerHTML = "O"
+    else if (board[i] === "X") {
+        boxElement.innerHTML = '<img src="./img/X.png" height="50px"/>'
     }
     else {
         boxElement.innerHTML = ""
@@ -16,3 +17,20 @@ function rendorBoard() {
    });
 }
 rendorBoard()
+
+function selectBox(boxNumber) {
+    if (board[boxNumber] !== -1) {
+        alert("Invalid Move..!")
+        return 
+    }
+
+    board[boxNumber] = currentPlayer
+    if (currentPlayer === "X") {
+        currentPlayer = "O"
+        document.getElementById("currentPlayer").innerHTML = '<img src="./img/O.png" height="15px">'
+    } else {
+        currentPlayer = "X"
+        document.getElementById("currentPlayer").innerHTML = '<img src="./img/X.png" height="15px">'
+    }
+    rendorBoard()
+}
