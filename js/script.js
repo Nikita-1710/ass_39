e1ad16dd70fd1ffaf7f1fb0d3f6b5ff69c9dbf11
Nikita-1710 <chausalkarnikita@gmail.com>
2024-12-25 15:21:45 +0530

    player winning function created

--- js/script.js
+++ js/script.js
@@ -25,6 +25,8 @@ function selectBox(boxNumber) {
     }
 
     board[boxNumber] = currentPlayer
+    rendorBoard()
+
     if (currentPlayer === "X") {
         currentPlayer = "O"
         document.getElementById("currentPlayer").innerHTML = '<img src="./img/O.png" height="15px">'
@@ -32,5 +34,72 @@ function selectBox(boxNumber) {
         currentPlayer = "X"
         document.getElementById("currentPlayer").innerHTML = '<img src="./img/X.png" height="15px">'
     }
+    chkWinner()
+}
+
+function isPlayerPresent(places, player) {
+   const a = places[0]
+   const b = places[1]
+   const c = places[2]
+   if(board[a] == player && board[b] == player && board[c] == player) {
+    return true
+   } else {
+    return false
+   }
+}
+
+function chkWinner() {
+    /*
+        Winning Positions
+        0,1,2
+        3,4,5
+        6,7,8
+        0,3,6
+        1,4,7
+        2,5,8
+        0,4,8
+        2,4,6
+    */
+   if (isPlayerPresent([0,1,2], "X")) {
+    alert("Player X wins")
+   } else if (isPlayerPresent([3,4,5], "X")) {
+    alert("Player X wins")
+   } else if (isPlayerPresent([6,7,8], "X")) {
+    alert("Player X wins")
+   } else if (isPlayerPresent([0,3,6], "X")) {
+    alert("Player X wins")
+   } else if (isPlayerPresent([1,4,7], "X")) {
+    alert("Player X wins")
+   } else if (isPlayerPresent([2,5,8], "X")) {
+    alert("Player X wins")
+   } else if (isPlayerPresent([0,4,8], "X")) {
+    alert("Player X wins")
+   } else if (isPlayerPresent([2,4,6], "X")) {
+    alert("Player X wins")
+   } 
+
+   if (isPlayerPresent([0,1,2], "O")) {
+    alert("Player O wins")
+   } else if (isPlayerPresent([3,4,5], "O")) {
+    alert("Player O wins")
+   } else if (isPlayerPresent([6,7,8], "O")) {
+    alert("Player O wins")
+   } else if (isPlayerPresent([0,3,6], "O")) {
+    alert("Player O wins")
+   } else if (isPlayerPresent([1,4,7], "O")) {
+    alert("Player O wins")
+   } else if (isPlayerPresent([2,5,8], "O")) {
+    alert("Player O wins")
+   } else if (isPlayerPresent([0,4,8], "O")) {
+    alert("Player O wins")
+   } else if (isPlayerPresent([2,4,6], "O")) {
+    alert("Player O wins")
+   } 
+}
+
+function resetGame() {
+    board.fill(-1)
     rendorBoard()
+    currentPlayer = "X"
+    document.getElementById("currentPlayer").innerHTML = '<img src="./img/X.png" height="15px">'
 }
